ace00d6665c846066688c8d1af958a222d62f756
sumukhj1219 <sumukhjoshi75@gmail.com>
2024-11-06 22:15:32 +0530

    modified disks

--- components/ui/RotatingDisc.js
+++ components/ui/RotatingDisc.js
@@ -15,16 +15,23 @@ const RotatingDisc = (props) => {
 
   return (
     <div 
-      className="size-96 rounded-full border-yellow-900 border-8 border-double shadow-lg shadow-yellow-900 transition-transform duration-100 ease hover:grayscale" 
+      className="size-96 rounded-full transition-transform duration-100 ease hover:grayscale relative flex items-center justify-center" 
       style={{
         backgroundImage: `url(${imgUrl || '/honeysingh.jpg'})`, 
-        size: `${'96' || size}`,
         backgroundSize: 'cover', 
         transform: `rotate(${rotate}deg)`,
-        border:`${border || '8px double yellow-900'}`,
-        shadow: `${shadow || 'shadow-lg yellow-900'}`
+        border: border || '8px double #a16207',
+        boxShadow: shadow || '0px 0px 15px rgba(255, 204, 0, 0.6)',
       }}
     >
+      <div
+        className="size-24 rounded-full bg-gradient-to-r from-black from-10% via-gray-900 via-30% to-black to-90% m-2 max-w-7xl items-center justify-center mx-auto"
+        style={{
+          position: 'absolute',
+          border: border || '8px double #a16207',
+          boxShadow: shadow || '0px 0px 15px rgba(255, 204, 0, 0.6)',
+        }}
+      />
     </div>
   );
 };
