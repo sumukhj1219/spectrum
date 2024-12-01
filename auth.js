e18bc28be534cac8927ea6adb6b1d369d601b2b6
sumukhj1219 <sumukhjoshi75@gmail.com>
2024-12-01 23:11:19 +0530

    v1.3

--- auth.js
+++ auth.js
@@ -1,5 +1,5 @@
 import NextAuth from "next-auth";
-import SpotifyProvider from "next-auth/providers/spotify";
+import Spotify from "next-auth/providers/spotify";
 import { Buffer } from "buffer"; // If you need to encode credentials
 import prisma from "./utils/db";
 // Define Spotify scopes
